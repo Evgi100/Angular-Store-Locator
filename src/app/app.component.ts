1b549d809314b7c24d685f7d1624451d60978b7c
Evgi100 <evgeni.shapira@gmail.com>
2017-12-27 10:37:08 +0200

    first commit-Map and view data are ready ,no algoritim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { StoresService } from './stores.service'
+import { OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms'
+import { ElementRef } from '@angular/core';
+import { MapsAPILoader } from '@agm/core';
+import { } from 'googlemaps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public latitude: number;
+  public longitude: number;
+  public zoom: number;
+  public locationChosen = false;
+  public stores: string;
+
+  public searchControl: FormControl;
+
+  @ViewChild("search")
+  public searchElementRef: ElementRef
+
+
+  constructor(private storeService: StoresService, private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) { }
+
+  ngOnInit() {
+    this.zoom = 13;
+    this.latitude = 32.0853;
+    this.longitude = 34.7818;
+    this.searchControl = new FormControl();
+
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+          this.locationChosen = true;
+        });
+      });
+    });
+  }
+
+  onMapClick(event) {
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+    this.locationChosen = true;
+  }
+
+  onSubmit() {
+    // this.storeService.fetchStores();
+    this.storeService.getStores().subscribe((data) => {
+      this.stores = data.chains;
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule }      from '@angular/http';
+import {AgmCoreModule} from '@agm/core';
+import{StoresService} from './stores.service'
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -10,9 +13,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyBGcyGU4ZX8p0K9Ba4qkRj9GDtvAhk_xxE',
+      libraries:["places" ]
+    })
   ],
-  providers: [],
+  providers:[StoresService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
